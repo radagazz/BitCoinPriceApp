d9aeca106348f9fc112b36adab460b5b28199011
Anurak Teerarattananukulchai <anurak.teera@gmail.com>
2021-01-13 23:35:58 +0700

    Finished !

---
+++ BitCoinPriceApp/Model/CoinData.swift
@@ -0,0 +1,12 @@
+//
+//  CoinData.swift
+//  BitCoinPriceApp
+//
+//  Created by anurak teerarattananukulchai on 13/1/2564 BE.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
